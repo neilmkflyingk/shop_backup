047ecb37d1fa796717b3bd3f4be4546896264fc7
neilmkflyingk <126199320+neilmkflyingk@users.noreply.github.com>
2023-09-08 11:52:27 -0700

    product.delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -106,13 +106,28 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
-      res.status(400).json(err);
+      //console.log(err);
+      //res.status(400).json(err);
+      res.json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!productData) {
+      res.status(404).json({message: 'No Product found with that ID'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
