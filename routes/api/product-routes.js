0b0aa9b6ca1db9fee0e4fadb618816408b59863b
neilmkflyingk <126199320+neilmkflyingk@users.noreply.github.com>
2023-09-08 11:48:25 -0700

    product.getone route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,9 +18,23 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category}, { model: Tag, attributes: ['tag_name'], through: ProductTag, as: 'productTag_products'}]
+    });
+
+    if (!productData) {
+      res.status(400).json({ message: 'No Product found with this id' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
