5b4585e57fdb6e177f19784b24a583a67e520e23
neilmkflyingk <126199320+neilmkflyingk@users.noreply.github.com>
2023-09-08 13:18:15 -0700

    tag.getOne route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,9 +16,22 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'productTag_products' }]
+    });
+    if (!tagData) {
+      res.status(400).json({ message: 'No Product found with this id' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
